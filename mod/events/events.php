41336cf7fa4ebe82b496a1d2a500de5185463d63
Gio Borje <gccielo@gmail.com>
2013-01-14 13:52:19 -0800

    Updated events for Winter quarter

---
+++ mod/events/events.php
@@ -0,0 +1,30 @@
+<h2>Events</h2>
+<article>
+	<footer>Week 2: Thursday <a
+	href="http://www.uci.edu/campusmap/map.php?l=1&q=113"
+	target="_blank">@CSL15</a></footer>
+	<h3>Algorithm Practice 1A</h3>
+	<p>First competition practice sesions for HackerRank.</p>
+</article>
+<article>
+	<footer>Week 2: Thursday <a
+	href="http://www.uci.edu/campusmap/map.php?l=1&q=113"
+	target="_blank">@CSL15</a></footer>
+	<h3>Algorithm Practice 1B</h3>
+	<p>First competition practice sesions for HackerRank.</p>
+</article>
+<article>
+	<footer>Week 3: Thursday <a
+	href="http://www.uci.edu/campusmap/map.php?l=1&q=113"
+	target="_blank">@CSL15</a></footer>
+	<h3>Algorithm Practice 2A</h3>
+	<p>Second competition practice sesions for HackerRank.</p>
+</article>
+<article>
+	<footer>Week 3: Thursday <a
+	href="http://www.uci.edu/campusmap/map.php?l=1&q=113"
+	target="_blank">@CSL15</a></footer>
+	<h3>Algorithm Practice 2B</h3>
+	<p>Second competition practice sesions for HackerRank.</p>
+</article>
+
