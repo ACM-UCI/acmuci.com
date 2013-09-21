bbd5c824db86ecc55b4b933974bd82e0ea62dd03
Gio Borje <gccielo@gmail.com>
2013-09-21 16:25:46 -0700

    Updated website for 2013-14 academic year

--- common.php
+++ common.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 define('APP_ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 define('APP_LIBS', APP_ROOT . 'libs' . DIRECTORY_SEPARATOR);
@@ -9,40 +9,42 @@ require_once APP_ROOT . 'config.php';
 $dsn = sprintf('%s:%s', DB_ENGINE, APP_LIBS . 'db/' . DB_NAME);
 $db = new PDO($dsn);
 
-require_once APP_LIBS . 'facebook/facebook.php';
-
-$fb = new Facebook(array(
-	'appId' => FB_APP_ID,
-	'secret' => FB_SECRET
-));
-
 if (!session_id())
 	session_start();
 
-$user = $fb->getUser();
-$logged_in = false;
+if ($_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+	require_once APP_LIBS . 'facebook/facebook.php';
 
-if (!$user) {
-	$login_url = $fb->getLoginUrl(array(
-		'scope' => 'create_event',
-		'redirect_uri' => 'http://' . $_SERVER['HTTP_HOST'] . '/' .
-		$_SERVER['REQUEST_URI']
+	$fb = new Facebook(array(
+		'appId' => FB_APP_ID,
+		'secret' => FB_SECRET
 	));
-} else if (empty($_SESSION['user_info'])) {
-	$logged_in = true;
-	$user_info = $fb->api("/$user");
 
-	$query = 'SELECT *
-		FROM members
-		WHERE member_fb_id = :user_id';
-	$stmt = $db->prepare($query);
-	$stmt->bindParam(':user_id', $user_info['id']);
-	$stmt->execute();
-	$member = $stmt->fetch();
-	$user_info = array_merge($user_info, $member);
-	$_SESSION['user_info'] = $user_info;
-} else {
-	$user_info = $_SESSION['user_info'];
+	$user = $fb->getUser();
+	$logged_in = false;
+
+	if (!$user) {
+		$login_url = $fb->getLoginUrl(array(
+			'scope' => 'create_event',
+			'redirect_uri' => 'http://' . $_SERVER['HTTP_HOST'] . '/' .
+			$_SERVER['REQUEST_URI']
+		));
+	} else if (empty($_SESSION['user_info'])) {
+		$logged_in = true;
+		$user_info = $fb->api("/$user");
+
+		$query = 'SELECT *
+			FROM members
+			WHERE member_fb_id = :user_id';
+		$stmt = $db->prepare($query);
+		$stmt->bindParam(':user_id', $user_info['id']);
+		$stmt->execute();
+		$member = $stmt->fetch();
+		$user_info = array_merge($user_info, $member);
+		$_SESSION['user_info'] = $user_info;
+	} else {
+		$user_info = $_SESSION['user_info'];
+	}
 }
 
 ?>
